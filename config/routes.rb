1f00b7cd0a159a3f1c8851ea0601cfc021d76dfc
Kate Wood <katewood611@gmail.com>
2015-07-01 11:33:19 -0700

    adds routes for users, conversations, messages and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'conversations/create'
+  resources :users
 
-  get 'conversations/show'
-
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/create'
+  resources :conversations do
+    resources :messages
+  end
 
   get 'sessions/new'
 
@@ -21,7 +17,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
