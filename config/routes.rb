da8f2620244e43b7008a634d06e679a735532db1
Taylorpadams <taylorpadams@gmail.com>
2015-07-01 10:26:56 -0700

    adds session controller with full CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/show'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
