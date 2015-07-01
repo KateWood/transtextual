4330db3ff6149c23ac55b5ad0e31313c1ee456ba
Kate Wood <katewood611@gmail.com>
2015-07-01 13:22:22 -0700

    adds fb_id to users table

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,4 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def show
     redirect_to root_path unless session['auth']
     @auth = session['auth']
@@ -11,8 +8,31 @@ class SessionsController < ApplicationController
   def create
     # Fetches the OAuth response info
     @auth = request.env['omniauth.auth']
-    session['auth'] = @auth
-    redirect_to sessions_show_path
+    if User.where(:spotify_id => @auth["info"]["id"]).first
+      @user = User.where(:spotify_id => @auth["info"]["id"]).first
+    else
+      if @auth["info"]["images"][0]["url"] == ""
+        @user = User.create(
+        :display_name => @auth["info"]["display_name"],
+        :token => @auth["credentials"]["token"],
+        :refresh_token => @auth["credentials"]["refresh_token"],
+        :image_url => 'muziqala_logo.jpq',
+        :spotify_id => @auth["info"]["id"],
+        :user_uri => @auth["info"]["uri"]
+        )
+      else
+      @user = User.create(
+        :display_name => @auth["info"]["display_name"],
+        :token => @auth["credentials"]["token"],
+        :refresh_token => @auth["credentials"]["refresh_token"],
+        :image_url => @auth["info"]["images"][0]["url"],
+        :spotify_id => @auth["info"]["id"],
+        :user_uri => @auth["info"]["uri"]
+        )
+      end
+    end
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701173334) do
+ActiveRecord::Schema.define(version: 20150701202113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150701173334) do
     t.string   "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "fb_id"
   end
 
   add_foreign_key "messages", "conversations"
